acb12f38a79f09899b70c0caed56a7308784b066
dnlsfq <dan90@live.com.my>
2020-01-25 01:16:07 +0800

    'updated'

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var randomNumGenerator = num => {
+    return Math.floor(Math.random() * num ) + 1 // 0 to Num
+};
+
+var randomNum = randomNumGenerator(5);
+var counterNum = 0;
+var count = 0;
+document.write('The random number was ' + randomNum);
+while(counterNum != randomNum ){
+    counterNum = randomNumGenerator(5);
+    count += 1;
+    if(counterNum == randomNum){
+        document.write('<p>It took the computer ' + count + ' attempts to get it right.</p>');
+        break;
+    }
+}
